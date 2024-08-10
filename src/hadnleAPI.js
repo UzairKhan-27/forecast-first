a4974da0b474450c1833217270c9f743e47c2fb5
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-10 16:09:24 +0500

    Hit Whether API

---
+++ src/hadnleAPI.js
@@ -0,0 +1,35 @@
+async function getLocation(city) {
+	const response = await fetch(
+		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=Q42L8M379A7KWKU3A6AMDPVFG`,
+		{ mode: "cors" },
+	);
+	const whetherData = await response.json();
+	const whetherRecord = createWhetherObject(whetherData);
+	return whetherRecord;
+}
+
+function createWhetherObject(whetherData) {
+	// console.log(whetherData);
+	// const whetherObject = {
+	// 	temperature: whetherData.currentConditions.temp,
+	// 	humidity: whetherData.currentConditions.humidity,
+	// 	feelsLike: whetherData.currentConditions.feelslike,
+	// 	currentTime: whetherData.currentConditions.datetime,
+	// 	records: whetherData.days[0],
+	// };
+	// console.log(whetherObject);
+
+	const whetherRecord = whetherData.days.map((day) => ({
+		temperature: day.temp,
+		maxTemperature: day.tempmax,
+		minTemperature: day.tempmin,
+		humidity: day.humidity,
+		feelsLike: day.feelslike,
+		condition: day.conditions,
+		description: day.description,
+		icon: day.icon,
+	}));
+	return whetherRecord;
+}
+
+export { getLocation };
