874fcef4f2596655540d148d094afeca5ae8e42f
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-11 16:23:22 +0500

    Call attach event listener

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
+import { attachEventListener } from "./DOM";
 import "./style.css";
 
 console.log("hello forecast");
-document.addEventListener("click", () => {
-	const textNode = document.createElement("p");
-	textNode.textContent = "HELLLLOO";
-	const body = document.querySelector("dasdsa");
-	body.appendChild(textNode);
-});
+
+attachEventListener();
