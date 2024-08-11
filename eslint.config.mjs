229a539d6822b3b95edd78ce703a4d5e46d64345
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-08-11 17:08:17 +0500

    Project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,20 @@
 import pluginJs from "@eslint/js";
 
 export default [
-  pluginJs.configs.recommended,
+	pluginJs.configs.recommended,
 
-  {
-    languageOptions: {
-      globals: {
-        console: "readonly",
-        document: "readonly",
-        window: "readonly",
-      },
-    },
-    rules: {
-      semi: "error",
-      // Add more rules as needed
-    },
-  },
+	{
+		languageOptions: {
+			globals: {
+				console: "readonly",
+				document: "readonly",
+				window: "readonly",
+				fetch: "readonly",
+			},
+		},
+		rules: {
+			semi: "error",
+			// Add more rules as needed
+		},
+	},
 ];
